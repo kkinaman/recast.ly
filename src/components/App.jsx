8241c3e99edd587a3219bc83de3dc5aceb642d2a
kkinaman <kkinaman419@gmail.com>
2016-09-30 15:51:41 -0700

    Implement video player, video list based on hardcoded data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,31 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      curVidId: props.videoData[0].id.videoId
+    };
+  }
+
+  // clickHandler(e) {
+  //   this.state
+  // }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer curVidId={this.state.curVidId} video={this.props.videoData[0]}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList curVidId={this.state.curVidId} videos={this.props.videoData}/>
+        </div>
+      </div>
+    );
+  } 
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
